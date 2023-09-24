e13db286dbe76504204a31c1d30fdce94af4e8e5
Faizan Ashraf <134432457+faizan786u@users.noreply.github.com>
2023-09-24 06:06:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,338 @@
+
+// CHAPTER # 1 (Alert)
+
+// Ques 1. Alert these following (individually):
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number
+// V. Password
+
+// alert(" My First name is Faizan ");
+// alert(" My last name is Ashraf ");
+// alert(" My email is shariqshah305@gmail.com ");
+// alert(" My phone number is 03152871062 ");
+// alert(" My password is 12345 ");
+
+// 2. Correct this statement: alert"You're learning JavaScript!";
+//  alert('You are learning javascript');
+
+// 3. Code an alert statement displaying any message you like.
+//  alert('yes I m learning javascript ')
+
+
+ // CHAPTER # 2 (VARIABLES FOR STRING)
+
+// 1. var name = "My name is Faizan ashraf" ; 
+//   var nationality = " and  nationality is Pakistani";
+
+//  var print= name +  nationality;
+//  alert(print);
+
+ // 2. var a = "I am study in " ;
+ //  var b =  " Learning Javascript" ;
+ //  var c = a + b;
+
+ //  alert(c);
+
+// 3. var TeamName = "Concept Faizan";
+//  alert(TeamName);
+  
+ // 4. var bestMan=" Charlie";
+ // alert(bestMan);
+
+
+// Chapter # 3 (Variables for numbers)
+
+// 1. Declare a variable “caseQty”
+// var a = 20;
+// var b=  23;
+// var c = a + b;
+// alert(c);
+
+//2. Assign to the variable caseQty, which has already been declared, the value 144.
+  
+  // var x = 72;
+  // var y = 72;
+  // var z = x + y;
+
+  // alert(z);
+
+ // 3. Rewrite this statement so the variable can be used in a math operation. var num = "9";
+
+    // var num1 = 3;
+    // var num2 = 3;
+    // var tnum = num1 * num2;
+
+    // alert(tnum);
+
+// 4. In one statement declare a variable. In a second statement assign it the sum of 2 numbers.    
+
+      // var j = 10;
+      // var i = 8;
+      // var k = j - i;
+
+      // alert(k
+
+// 5. What is the value of orderTotal?
+
+
+      // var merchTotal = 100;
+      // var shippingCharge = 10;
+      // var orderTotal = merchTotal + shippingCharge;
+        
+      // alert(orderTotal);  
+
+  // 6. In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number.    
+
+        // var  num1 = 200;
+        // var  num2 = 10;
+        // var  numT = num1 + 10;
+        // alert(numT);
+
+
+  // Chapter # 4 (Variable names Legal and Illegal)      
+
+    // 1. Correct this statement.
+       // var product cost = 3.45;
+
+
+          // var product_cost = 3.45;
+
+     // 2. Rewrite this using camelCase.
+     //    var Nameofband;     
+
+          // var Nameofband = "faizanAhraf";
+          // alert(Nameofband);
+        
+        // 3. In a single statement declare a legally-named variable and assign a number to it.
+          
+           // var firstname123 = 4;
+           // var first_number = 10;
+           // var totalNumber  =  firstname123 + first_number;
+           // alert(totalNumber);
+         
+      // 4. Declare a variable that is a combination of your first and last
+            
+            // var firstName ="Faizan ";
+            // var lastName  = "Ashraf";
+            // var fullName  = firstName + lastName;
+            // alert(fullName);
+
+
+    // 5. List the legal and Illegal Variables.   
+           
+        // Legal Variables:
+
+        //   var firstName;
+        //   var first_Name;
+        //   var firstname;
+        //   var firstname123;
+    
+        // Ilegal Variables:
+
+        //   var 123firstname;
+        //   var first name;
+        //   var firstname$%&first;
+        //   var firstname@$%name;
+             // var first-name;
+
+        // Chapter # 5 (Math Expression I)    
+
+        // 1. What is the name and symbol of the arithmetic operator that gives you the remainder when one number is divided by another?
+       
+        // var divide = 100;
+        // var divide2 = 20;
+        // var answer  = divide / divide2 ;
+        // alert(answer);      
+
+        // 2. What is the value of num?
+        //    var num = 20 % 6;
+
+          // var num = 20% 6;
+          // var num1 = 20% 8;
+          // var Tnum= num / num1;
+          // alert(Tnum);   
+           
+    // 3. In a single statement, declare the variable largeNum and assign it the result of 1,000 multiplied by 2,000 
+
+          // var largeNum = 2000;
+          // var smallNum = 1;
+          // var Multiply = 2000 * 1 / 200;
+          // alert(Multiply);
+
+     // 4. Assign to a variable the value represented by one variable subtracted from the value represented by another variable     
+
+         // var value1 = 100;
+         // var value2 = 80;
+         // var Subt   = value1 - value2 ;
+         // alert(Subt);
+
+       // 5. Assign to a variable the remainder when one number is divided by another. The variable hasn't been declared beforehand. Make up the variable name.  
+
+          // var four = 4 ;
+          // var two  = 2;
+          // var ans  = four / two ;
+          // alert(ans);
+
+// 6. Code an alert that displays the result of a multiplication on 2 numbers.
+            
+            // var mul = 5;
+            // var mul1 = 5;
+            // var ans = mul * mul1;
+            // alert(ans);
+
+
+   // Chapter #6 (Math Expression II)
+
+   // 1. Code a short form of x = x + 1; Use either of the two legal expressions
+
+      // 2. If x has a value of 100, what is the fastest way to reduce it to 99 with a math expression?
+
+      // var x = 100;
+      // var result = x--;
+      // alert( x);
+ 
+      // 3. var x = 50;
+      //     var y = x++; 
+      //    What is the value of y?
+
+      // var x = 50;
+      // var y = x++;
+      // alert(y); 
+
+   // 4. var y = 50;
+   //     var z = --y;
+   //       What is the value of z?   
+
+       // var y = 50;
+       // var z = --y;
+       // alert(z);
+
+// 5. In a single statement, decrement num and assign its original value to newNum.
+
+      // var i = 20;
+      // var j = --i;
+      // alert(j);
+
+  // 6. In a single statement add 1 to a variable and assign its original value to another variable.    
+
+          // var a = 29;
+          // var b = a++;
+          // alert(a);
+
+//    7. Assign a number value to a variable.
+// Increment the variable.
+// Display the new value in an alert 
+     
+       // var num1 = 25;
+       // var num  = ++ num1;
+       // alert(num);
+  
+   // Chapter #7 (Math Expression III)
+   
+//     1. var calculatedNum = 2 + (2 * 6);
+// What is the value of calculatedNum?
+
+      // var calculatedNum = 2 + (2*6);
+      // alert(calculatedNum);
+   
+//       2. var calculatedNum = (2 + 2) * 6;
+// What is the value of calculatedNum?
+
+         // var calculatedNum = (2 + 2) * 6;
+         // alert(calculatedNum);
+        
+//         3. var calculatedNum = (2 + 2) * (4 + 2);
+// What is the value of calculatedNum?
+           
+            // var calculatedNum = (2 + 2) * (4 + 2);
+            // alert(calculatedNum);
+           
+//            4. var calculatedNum = ((2 + 2) * 4) + 2;
+// What is the value of calculatedNum?
+               
+               // var calculatedNum ((2 + 2) * 4) + 2;
+               // alert(calculatedNum);
+
+// 5. Write a statement that assigns to cost the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 56. 
+                
+                // var calculatedNum = 2 + 2 * 4 + 10;
+                // alert(calculatedNum);
+
+   // 6. Write a statement that assigns to units the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 20.  
+                  
+                  // var calculatedNum = 2 + 2 * 4 + 10;
+                  // alert(calculatedNum);    
+
+       // 7. Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing 5.                 
+
+
+                     // var calculatedNum = 4 / 2 * 4;
+                     // alert(calculatedNum);
+
+
+                  // Chapter #8 (Concatenating Text Strings)   
+
+// 1. var num = "2" + "2";
+// What is the value of num? Include quotation marks.
+
+                        // var num = "2" + "2";
+                        // alert(num);
+                        // result = 22
+
+//    2. message = ("Hello," + "Dolly");
+// What is the value of message? (Include the quotation marks.)   
+                          
+                          // var message = ("Hello", + "Dolly");
+                          // alert(message); 
+                          // result = NaN                
+
+//       3. alert("33" + 3);
+// What message displays in the alert box?                    
+                
+// 4. Write an alert that displays the concatenation of the two parts of "Pakistan Zindabad".
+                 
+                 // alert("Pakistan Zindabad");
+
+   // 5. Write a statement that assigns to a variable the concatenation of a string with a number
+
+                    // var num = ("22" + "3");
+                    // alert(num);      
+
+       // 6. Assign strings to two variables. Then concatenate them and assign the result to a third variable.  
+                       
+                      // var alpha = " Concept";
+                      // var Beta  =  "Faizan ";
+                      // var result = alpha  + Beta ;
+                      // alert(result);
+
+                   // Chapter #9 (Prompts)
+
+// 1. Code a prompt with the message "Enter first name". The user's response is assigned to firstName
+                      
+                      // prompt('Enter your first name');
+
+ // 2. Code a prompt with the message "Country?" and the default answer "China". The user's response is assigned to country.
+                        
+                        // prompt('Country');
+
+//     3. Correct this statement
+// var yourName = prompt(Enter Your Name");                    
+   
+                        // var yourName = prompt('enter your name');
+
+    // 4. Code a prompt that specifies a string as the message Include a default input                    
+
+                         // prompt("WELCOME TO KARACHI ");
+
+       // 5. Assign strings to two variables. Code a prompt specifying the first variable as the message and the second variable as the default response. Assign the user's response to a third variable.                  
+
+                            // var karachi = prompt('The city of light');
+                            // var edu = prompt('Fsc');
+
+//           6. Display a prompt, including both a message and a default response.
+// Display the user's response in an alert.
+                                
+                               prompt('Never give up');
+                               alert( "Yes offcourse");                    
